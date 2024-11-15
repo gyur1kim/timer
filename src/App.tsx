d97c1f0d0c3bc9775f086861163bfca949877a0b
gyur1kim <qri1114@gmail.com>
2024-11-16 03:04:13 +0900

    feat: timer에 시작 정지 버튼 생성

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <>
       <h2>타이머를 만드는 네 가지 방법</h2>
       <div className="wrapper">
-        <WrongTimer milliseconds={10000} />
+        <WrongTimer milliseconds={600000} />
       </div>
     </>
   );
--- src/timer/WrongTimer.tsx
+++ src/timer/WrongTimer.tsx
@@ -12,8 +12,11 @@ function WrongTimer({ milliseconds }: WrongTimerType) {
   const [time, setTime] = useState<number>(milliseconds);
   const [intervalId, setIntervalId] =
     useState<ReturnType<typeof setInterval>>();
+  const [run, setRun] = useState<boolean>(false);
 
   useEffect(() => {
+    if (!run) return;
+
     const handleTimer = () => {
       setTime((prev) => prev - 1000);
     };
@@ -25,22 +28,35 @@ function WrongTimer({ milliseconds }: WrongTimerType) {
     setIntervalId(intervalId);
 
     return () => {
-      if (intervalId) clearInterval(intervalId);
+      if (run && intervalId) clearInterval(intervalId);
     };
-  }, []);
+  }, [run]);
 
   useEffect(() => {
-    if (time <= 0 && intervalId) {
+    if (time <= 0 && run && intervalId) {
       clearInterval(intervalId);
+      setRun(false);
     }
-  }, [time, intervalId]);
+  }, [time, intervalId, run]);
 
   const [min, sec] = timerFormat(time);
 
   return (
-    <div className="timer-wrapper">
-      <span className="time">{min}</span>:<span className="second">{sec}</span>
-    </div>
+    <>
+      <div className="timer-wrapper">
+        <span className="time">{min}</span>:
+        <span className="second">{sec}</span>
+      </div>
+      <div className="button-wrapper">
+        <button
+          type="button"
+          className={`button_${run ? "stop" : "go"}`}
+          onClick={() => setRun((prev) => !prev)}
+        >
+          {run ? "일시정지" : "시작하기"}
+        </button>
+      </div>
+    </>
   );
 }
 
