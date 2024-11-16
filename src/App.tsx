24de272ac5205c6a0c3e943c25bc007f64da6149
gyur1kim <qri1114@gmail.com>
2024-11-16 18:26:01 +0900

    feat: 탭 버튼으로 렌더링할 타이머 컴포넌트 이동
    - 이벤트 위임

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
-import "./App.css";
-import DelayedTimer from "./timer/timer/DelayedTimer";
+import { useState, MouseEvent } from "react";
+
+import DelayedTimer from "@timerComponents/DelayedTimer";
+import SetInterval from "@timerComponents/SetIntervalTimer";
+
+import "App.css";
+
+const timerTime = 600000;
+const timerList: Record<string, JSX.Element> = {
+  delay: <DelayedTimer milliseconds={timerTime} />,
+  setInterval: <SetInterval milliseconds={timerTime} />,
+};
 
 function App() {
+  const [action, setAction] = useState("delay");
+
+  const handleTab = (e: MouseEvent<HTMLDivElement>) => {
+    if (!(e.target instanceof HTMLButtonElement)) return;
+
+    const action = e.target.dataset.action;
+    if (action) {
+      setAction(action);
+    }
+  };
+
   return (
     <>
       <h2>타이머를 만드는 네 가지 방법</h2>
-      <div className="wrapper">
-        <DelayedTimer milliseconds={600000} />
+      <div className="tap-wrapper" onClick={(e) => handleTab(e)}>
+        <button data-action="delay">delay되는 타이머</button>
+        <button data-action="setInterval">setInterval로 구현한 타이머</button>
       </div>
+      <div className="wrapper">{timerList[action]}</div>
     </>
   );
 }
