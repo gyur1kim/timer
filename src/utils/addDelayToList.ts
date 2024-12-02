886c44a2914b49c471f0ba8a6bb7af12f2b4ce17
gyur1kim <qri1114@gmail.com>
2024-12-02 22:16:42 +0900

    fix: requestAnimationFrame에서 점점 delay가 늦어지는 이슈 해결

--- src/utils/addDelayToList.ts
+++ src/utils/addDelayToList.ts
@@ -2,6 +2,9 @@ function addDelayToList(delay: number) {
   const ulElement = document.querySelector(".delay-list")!;
   const li = document.createElement("li");
 
+  if (ulElement.children.length > 50) {
+    ulElement.removeChild(ulElement.firstChild!);
+  }
   li.innerText = `[delay] ${delay}`;
   ulElement.appendChild(li);
   ulElement.scrollTop = ulElement.scrollHeight;
